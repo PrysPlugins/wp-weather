2d9af368c3dd3f931f606bd202ab42ec10822519
Jeremy Pry <jeremy.pry@gmail.com>
2014-06-17 10:40:17 -0400

    Build out more methods

--- class-forecast.php
+++ class-forecast.php
@@ -14,14 +14,19 @@ class Forecast_IO {
 	private $api_key;
 	
 	/**
-	 *
+	 * API constants.
 	 */
-	const API_URL = 'https://api.forecast.io/';
+	const API_SCHEME = 'https';
+	const API_DOMAIN = 'api.forecast.io';
+	const API_PATH   = '/forecast/';
+
+	/* Public Methods
+	-----------------------------------------------*/
 	
 	/**
 	 * Get the single instance of this class.
 	 *
-	 * @since 1.0
+	 * @since 0.1.0
 	 *
 	 * @return Forecast_IO The class object.
 	 */
@@ -35,18 +40,94 @@ class Forecast_IO {
 		}
 		return $instance;
 	}
+
+	/**
+	 * Get data from the API.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $latitude  The latitude of weather information to request
+	 * @param string $longitude The longitude of weather information to request
+	 * @param string $time      (Optional) The timestamp of weather information to request
+	 * @return stdClass The weather data object
+	 */
+	public function get( $latitude, $longitude, $time = null ) {
+		return $this->curl_request( 'get', $latitude, $longitude, $time );
+	}
+
+	/**
+	 * Get data from the API
+	 *
+	 * Allows additional options to be sent in addition to the normal settings.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $latitude  The latitude of weather information to request
+	 * @param string $longitude The longitude of weather information to request
+	 * @param string $options   Options to send with the request.
+	 * @param string $time      (Optional) The timestamp of weather information to request
+	 * @return stdClass
+	 */
+	public function post( $latitude, $longitude, $options, $time = null ) {
+		return $this->curl_request( 'post', $latitude, $longitude, $time, $options );
+	}
+
+	/* Protected Methods
+	-----------------------------------------------*/
 	
 	/**
 	 * Constructor.
 	 * 
 	 * Set up the API Key
 	 * 
-	 * @since 1.0
+	 * @since 0.1.0
 	 * 
 	 * @param string $api_key The API Key to use when requesting data.
 	 */
 	protected function __construct( $api_key ) {
 		$this->api_key = $api_key;
+	}
+
+	/* Private methods
+	-----------------------------------------------*/
+
+	/**
+	 * Send a request to the API using CURL.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $method    The method to use when connecting to the server. Valid methods are 'get' or 'post'
+	 * @param string $latitude  The latitude of weather information to request
+	 * @param string $longitude The longitude of weather information to request
+	 * @param string $time      (Optional) The timestamp of weather information to request
+	 * @param array  $options   (Optional) Options to send with the request.
+	 * @return stdClass The weather data object
+	 */
+	private function curl_request( $method, $latitude, $longitude, $time = null, $options = array() ) {
+
+		// Build the URL
+		$url = "${self::API_SCHEME}://${self::API_DOMAIN}${self::API_PATH}{$this->api_key}/{$latitude},{$longitude}";
+
+		if ( null !== $time ) {
+			$url .= ",{$time}";
+		}
+
+		// Set up curl
+		$c = curl_init( $url );
+		curl_setopt( $c, CURLOPT_FOLLOWLOCATION, true );
+		curl_setopt( $c, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $c, CURLOPT_CONNECTTIMEOUT, 15 );
+		curl_setopt( $c, CURLOPT_TIMEOUT, 20 );
+
+		// Handle POST
+		if ( 'post' == $method ) {
+
+		}
+
+		// Send the request
+		$response = curl_exec( $c );
+
+		return json_decode( $response );
 	}
 	
 	
